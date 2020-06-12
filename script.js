e98c25b3b3695a86b583465b8dbe9b441a7ee760
Rudolph845 <59322684+Rudolph845@users.noreply.github.com>
2020-06-12 16:20:45 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('nav-bar-list')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
